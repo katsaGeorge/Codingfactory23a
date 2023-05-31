5b6367c6e775759b43ff0d14fa15db49bb2a6e42
katsaGeorge <georgekatsabsn@gmail.com>
2023-05-31 11:47:02 +0300

    Chapter 10 v0.2

--- src/gr/aueb/cf/ch10/TestForMe.java
+++ src/gr/aueb/cf/ch10/TestForMe.java
@@ -5,10 +5,12 @@ public class TestForMe {
 
     public static void main(String[] args) {
         StringBuilder in = new StringBuilder();
+        double a = 2.456;
+        double b = 3.56789;
 
         String s = "Coding Factory";
 
-
+        System.out.println(Math.ceil(b/a));
             System.out.print(change(in,s));
     }
     public static String change(StringBuilder in, String s){
